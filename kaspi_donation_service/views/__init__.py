0dcaa64d4c9c8290cd75303d1d65b65df1323f13
kabimollaev <72268779+kabimollaev@users.noreply.github.com>
2025-09-29 09:42:10 +0000

    update

---
+++ kaspi_donation_service/views/__init__.py
@@ -0,0 +1,9 @@
+# Этот файл сообщает Python, что папка 'views' является пакетом
+# и делает доступными модули для импорта в основном приложении.
+
+from . import main
+from . import auth
+from . import admin
+from . import api
+from . import widgets
+
